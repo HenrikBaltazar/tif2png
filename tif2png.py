c21ac69ba8837c905c13da1a70fd9e6cb20758ce
HenrikBaltazar <henrikgomesbaltazar@gmail.com>
2023-05-05 20:39:49 -0300

    feat headless

--- tif2png.py
+++ tif2png.py
@@ -3,6 +3,7 @@ import shutil
 import pathlib
 import tkinter as tk
 from tkinter import filedialog, messagebox
+import sys
 
 def convert_folder(folder_path):
     # Obter lista de arquivos TIFF na pasta selecionada
@@ -10,12 +11,13 @@ def convert_folder(folder_path):
     num_files = len(tif_files)
 
     # Desabilitar botão e campo de seleção de pasta durante a conversão
-    folder_button.config(state=tk.DISABLED)
-    folder_entry.config(state=tk.DISABLED)
+    if (not HEADLESS):
+        folder_button.config(state=tk.DISABLED)
+        folder_entry.config(state=tk.DISABLED)
 
     # Exibir animação de "loading"
-    loading_label = tk.Label(root, text='Converting...', font=('Helvetica', 16, 'bold'))
-    loading_label.pack(pady=20)
+        loading_label = tk.Label(root, text='Converting...', font=('Helvetica', 16, 'bold'))
+        loading_label.pack(pady=20)
 
     # Renomear arquivos substituindo espaços em branco por underscores
     for i, filename in enumerate(tif_files):
@@ -31,10 +33,11 @@ def convert_folder(folder_path):
     for i, filename in enumerate(glob.glob(f'{folder_path}/*.png')):
         shutil.move(filename, pathlib.Path(folder_path) / pathlib.Path(filename).name)
 
-    # Remover animação de "loading" e exibir mensagem de sucesso
-    loading_label.pack_forget()
-    messagebox.showinfo('Conversão Concluída', 'A conversão de arquivos foi concluída com sucesso.')
-    root.destroy()
+    if (not HEADLESS):
+        # Remover animação de "loading" e exibir mensagem de sucesso
+        loading_label.pack_forget()
+        messagebox.showinfo('Conversão Concluída', 'A conversão de arquivos foi concluída com sucesso.')
+        root.destroy()
 
 
 def browse_folder():
@@ -43,23 +46,31 @@ def browse_folder():
         folder_path_var.set(folder_path)
         convert_folder(folder_path)
 
+HEADLESS = False
 
-root = tk.Tk()
-root.title('CAIO - Conversor de Arquivos Instantaneo e Organizado')
-root.geometry('400x200')
+if len(sys.argv) == 1:
+    root = tk.Tk()
+    root.title('CAIO - Conversor de Arquivos Instantaneo e Organizado')
+    root.geometry('400x200')
 
-folder_path_var = tk.StringVar()
+    folder_path_var = tk.StringVar()
 
-folder_label = tk.Label(root, text='Selecione a pasta contendo os arquivos TIF:')
-folder_label.pack(pady=10)
+    folder_label = tk.Label(root, text='Selecione a pasta contendo os arquivos TIF:')
+    folder_label.pack(pady=10)
 
-folder_frame = tk.Frame(root)
-folder_frame.pack()
+    folder_frame = tk.Frame(root)
+    folder_frame.pack()
 
-folder_entry = tk.Entry(folder_frame, textvariable=folder_path_var, width=30)
-folder_entry.pack(side=tk.LEFT)
+    folder_entry = tk.Entry(folder_frame, textvariable=folder_path_var, width=30)
+    folder_entry.pack(side=tk.LEFT)
 
-folder_button = tk.Button(folder_frame, text='Selecionar', command=browse_folder)
-folder_button.pack(side=tk.LEFT, padx=10)
-
-root.mainloop()
+    folder_button = tk.Button(folder_frame, text='Selecionar', command=browse_folder)
+    folder_button.pack(side=tk.LEFT, padx=10)
+    root.mainloop()
+else:
+    HEADLESS = True
+    print("You are running headless, if you want a GUI run without argument")
+    if len(sys.argv) > 2:
+        print("Use the folder path as the only argument")
+    else:
+        convert_folder(sys.argv[1])
